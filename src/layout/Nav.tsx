b358dfd1a7c9cf5edc4779bd55078835497e657b
nmacl <80979544+nmacl@users.noreply.github.com>
2024-02-20 13:36:28 -0500

    Generated random wave data in the nav for the demo

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -1,17 +1,45 @@
 import { WavyLink } from "../library/waves";
 
-export const Nav = () => (
-  <nav>
-    <WavyLink to="/" color="#ff44fd">
-      Home
-    </WavyLink>
 
-    <WavyLink direction="up" to="/about" color="#8f44fd">
-      About
-    </WavyLink>
+// Add random data for more wave variance
 
-    <WavyLink duration={1000} to="/contact" color="#2f44fd">
-      Contact
-    </WavyLink>
-  </nav>
-);
+function generateRandomData(length: number): number[] {
+  return Array.from({ length }, () => Math.floor(Math.random() * 3) + 1);
+}
+
+export const Nav = () => {
+  // Only 4 for smooth waves
+  const seriesDataA = generateRandomData(4);
+  const seriesDataB = generateRandomData(4);
+
+  return (
+    <nav>
+      <WavyLink to="/" color="#ff44fd" 
+      seriesDataA={seriesDataA} 
+      seriesDataB={seriesDataB}>
+        Home
+      </WavyLink>
+
+      <WavyLink
+        direction="up"
+        to="/about"
+        color="#8f44fd"
+        seriesDataA={seriesDataA}
+        seriesDataB={seriesDataB}
+      >
+        About
+      </WavyLink>
+
+      <WavyLink
+        duration={1000}
+        to="/contact"
+        color="#2f44fd"
+        seriesDataA={seriesDataA}
+        seriesDataB={seriesDataB}
+      >
+        Contact
+      </WavyLink>
+    </nav>
+  );
+};
+
